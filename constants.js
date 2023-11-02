e2c9ad4c468dcb146f7e290762159774d454ad53
Jerome <ulohojerome@gmail.com>
2023-11-02 17:14:20 +0100

    change content

--- constants.js
+++ constants.js
@@ -1,12 +1,23 @@
 export const projects = [
   {
-    name: "Edekee",
-    link: "https://www.edekee.com",
+    name: "Cravve.ai Chrome Extension ",
+    link: "https://chrome.google.com/webstore/detail/cravveai/oklnlgogdhppeijnkplmhlecjlnnphba",
     image:
-      "https://firebasestorage.googleapis.com/v0/b/reddit-clone-49ba5.appspot.com/o/edekeephoto.png?alt=media&token=e12f62e1-c064-4214-b128-45afd63d2ebb",
-    technologies: ["Next.js", "ChakraUI", "Sass", "AWS"],
+      "https://firebasestorage.googleapis.com/v0/b/reddit-clone-49ba5.appspot.com/o/cravve.png?alt=media&token=d7f8d9d3-f913-491c-b159-1ae106a9d682&_gl=1*4q85g0*_ga*MjEwNzUwODU4Ni4xNjk1MTUyNTcx*_ga_CW55HF8NVT*MTY5ODk0MDUxOC4xNS4xLjE2OTg5NDEyOTQuNjAuMC4w",
+    technologies: ["Javascript", "Css", "Html"],
     description:
-      "This project showcases my pioneering work in the shoppable video space. Witness the seamless integration of captivating videos with interactive shopping experiences, paving the way for the future of e-commerce. 🚀💫",
+      "This dashboard exemplifies a meticulous attention to detail, providing a comprehensive and user-friendly interface for data visualization. 📈🧠",
+    github: "",
+  },
+ 
+  {
+    name: "Truelocal",
+    link: "https://truelocal.ng",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/reddit-clone-49ba5.appspot.com/o/Screenshot%202023-10-26%20213210.png?alt=media&token=3b1c828d-4766-49ec-adee-e1dccd0a8783&_gl=1*1tbio7y*_ga*MjEwNzUwODU4Ni4xNjk1MTUyNTcx*_ga_CW55HF8NVT*MTY5ODk0MDUxOC4xNS4xLjE2OTg5NDA2ODkuMzIuMC4w",
+    technologies: ["Next.js", "Material UI", "Tailwind", "redux", "vite"],
+    description:
+      "A dynamic platform that unites content creators, influencers, and clients in one seamless ecosystem. Connect, collaborate, and execute your creative projects effortlessly, with streamlined transactions and interactions all within this integrated platform.",
     github: "",
   },
   {
@@ -19,16 +30,7 @@ export const projects = [
       "This marketplace brings together creators and retailers in a vibrant platform, fostering fruitful collaborations and collective earning opportunities. 🤝✨. (Requires a passcode, available on request)",
     github: "",
   },
-  {
-    name: "Dashboard ",
-    link: "https://mene-oritsemiworon-lendsqr-fe-test.netlify.app/login",
-    image:
-      "https://firebasestorage.googleapis.com/v0/b/reddit-clone-49ba5.appspot.com/o/lendphoto.png?alt=media&token=f1876044-8468-4202-9fe9-a54f58d1efa5",
-    technologies: ["Reactjs", "Sass", "Netlify", "Typescript", "Vite"],
-    description:
-      "This dashboard exemplifies a meticulous attention to detail, providing a comprehensive and user-friendly interface for data visualization. 📈🧠",
-    github: "https://github.com/woronmene/lendsqr-fe-test",
-  },
+
   {
     name: "Shoppable video demo",
     link: "https://www.edekee.com/demo",
@@ -39,4 +41,15 @@ export const projects = [
       "This project demonstrates shoppable video technology on the web. Explore how products seamlessly intertwine with engaging videos, offering an innovative and immersive shopping experience.🌐✨",
     github: "",
   },
+  {
+    name: "Cravve.ai",
+    link: "https://www.cravve.ai",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/reddit-clone-49ba5.appspot.com/o/edekeephoto.png?alt=media&token=e12f62e1-c064-4214-b128-45afd63d2ebb",
+    technologies: ["Next.js", "ChakraUI", "Sass", "AWS"],
+    description:
+      "This project showcases my pioneering work in the shoppable video space. Witness the seamless integration of captivating videos with interactive shopping experiences, paving the way for the future of e-commerce. 🚀💫",
+    github: "",
+  },
+
 ];
